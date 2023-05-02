5599bc5e264e5372c1b17e00bcba16763c6d3116
BCD15 <bruno69correadias@gmail.com>
2023-05-02 16:38:32 -0300

    gradient

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
 import { StyleSheet, View } from 'react-native';
 import { Card } from 'react-native-paper';
-import { DefaultTheme } from 'react-native-paper';
 
 export default function Login () {
   return (
+    <LinearGradient
+      colors={['#000000', '#342348']}
+      style={{
+        flex: 1,
+      }}>
     <View style={styles.container}>
       <Card style={styles.card}>
         <Card.Title title="Login" />
       </Card>
     </View>
+    </LinearGradient>
   );
 }
 
@@ -17,7 +23,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: DefaultTheme.colors.background,
     alignItems: 'center',
     paddingTop: 10
   },

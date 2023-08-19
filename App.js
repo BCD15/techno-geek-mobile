1b4f0777df4154fd78945b0742838a94f02983e1
BCD15 <bruno69correadias@gmail.com>
2023-08-18 22:15:44 -0300

    fix app

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider as PaperProvider } from 'react-native-paper';
+
 import Home from './src/screens/Home';
 import Login from './src/screens/Login';
 import Cadastro from './src/screens/Cadastro';
 import Item from './src/screens/Item';
+import Carrinho from './src/screens/Carrinho';
+import { StatusBar } from 'expo-status-bar';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,11 +17,13 @@ export default function App() {
   return (
       <PaperProvider>
         <NavigationContainer>
+        <StatusBar hidden={true}/>
           <Stack.Navigator screenOptions={{ headerShown: false}}>
               <Stack.Screen name="Home" component={Home} />
               <Stack.Screen name="Login" component={Login} />
               <Stack.Screen name="Cadastro" component={Cadastro} />
               <Stack.Screen name="Item" component={Item} />
+              <Stack.Screen name="Carrinho" component={Carrinho} />    
           </Stack.Navigator>
         </NavigationContainer>
       </PaperProvider>

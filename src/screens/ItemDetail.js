e97d5b3a691db221c6384e388b621a2b25e1ab48
BCD15 <bruno69correadias@gmail.com>
2023-11-22 11:23:34 -0300

    .

--- src/screens/ItemDetail.js
+++ src/screens/ItemDetail.js
@@ -6,6 +6,7 @@ import {
   Text,
   Image,
   TouchableWithoutFeedback,
+  Pressable,
 } from "react-native";
 
 import voltarIcon from "../../assets/voltarIcon2.png";
@@ -15,24 +16,46 @@ export default function ItemDetail ({ navigation }) {
 
   return (
     <View style={{backgroundColor:"#fff", height: '100%',}}>
+
         <View style={styles.header}>
             <TouchableWithoutFeedback onPress={() => navigation.navigate("Home")}>
               <Image source={voltarIcon} style={styles.imageVoltar}></Image>
             </TouchableWithoutFeedback>
             <Text style={styles.titleHeader}>Camiseta de one piece black edition</Text>
         </View>
-        <ScrollView style={styles.main}>
-          <Image source={imageBlackEdition} style={styles.imageBlackEdition}></Image>
-          <Text style={styles.titleMain}>
-            Camiseta de one piece black edition
-          </Text>
-          <Text style={styles.textMain}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type 
-            specimen book.
-          </Text>
+
+        <ScrollView>
+
+          <View style={styles.main}>
+            <Image source={imageBlackEdition} style={styles.imageBlackEdition}></Image>
+            <Text style={styles.titleMain}>
+              Camiseta de one piece black edition
+            </Text>
+            <Text style={styles.textMain}>
+              A camiseta One Piece Dupla Face é um produto original, licenciado e exclusivo Piticas. Estampa inspirada na personagem
+              de mangá One Piece, as aventuras de as aventuras de Luffy e a sua tripulação, os Piratas do Chapéu de Palha, exploram a 
+              Grand Line em busca do tesouro mais procurado do mundo. Na Piticas, prezamos por qualidade, diversidade e conforto. 
+              A T-Shirt clássica, foi confeccionada em 100% algodão, conta com estilo autêntico, feita pelos nossos nerdsigners 
+              para vestir seu lado fã! Vista com orgulho, vista seu lado fã.
+            </Text>
+          </View>
+
         </ScrollView>
+
+          <View style={styles.footer}>
+            <Text style={{ color: '#9F9F9F', fontSize: 13, }}>
+              52 avaliações ⭐ ⭐ ⭐ ⭐ ⭐ 
+            </Text>
+            <Text style={styles.preco}>
+              R$18,90
+            </Text>
+            <Pressable>
+              <Text>
+                oi
+              </Text>
+            </Pressable>
+          </View>
+
     </View>
   );
 }
@@ -62,7 +85,7 @@ const styles = StyleSheet.create({
 
     imageBlackEdition: {
       width: '100%',
-      maxHeight: '35%',
+      maxHeight: '40%',
     },
 
     main: {
@@ -72,7 +95,7 @@ const styles = StyleSheet.create({
     titleMain: {
       color: "#6304AE",
       fontWeight: 'bold',
-      fontSize: 22,
+      fontSize: 23,
       marginLeft: 7,
       marginRight: 7,
       marginTop: 7,
@@ -80,9 +103,22 @@ const styles = StyleSheet.create({
 
     textMain: {
       color: "#9F9F9F",
-      fontSize: 15,
+      fontSize: 16,
       marginLeft: 7,
       marginRight: 7,
       marginTop: 4,
+    },
+
+    footer: {
+      flexDirection: 'row',
+      height: "10%",
+      marginLeft: 15,
+      marginRight: 15,
+      marginTop: 10,
+      justifyContent: 'space-between', 
+    },
+
+    preco: {
+      fontWeight: 'bold',
     }
 });
